5be3da43774534e02eb7a09784073e155e0f0051
wljyes <1757280079@qq.com>
2023-11-29 11:10:46 +0800

    通过控制上级路由器实现下载带宽限制

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from chrome_driver import ChromeDriver
-# import paramiko
+import paramiko
 import yaml
+import os
 
 configure_file = 'config.yaml'
 with open(configure_file, 'r', encoding='utf-8') as f:
@@ -9,27 +10,40 @@ with open(configure_file, 'r', encoding='utf-8') as f:
     net_interface = config['net_interface']
     play_resolutions = config['play_resolutions']
     fullscreen_play = config['fullscreen_play']
-    # net_cond_list = config['net_cond_list']
-    # ssh_host = config['ssh_host']
-    # ssh_port = config['ssh_port']
-    # ssh_username = config['ssh_username']
-    # ssh_password = config['ssh_password']
+    device_ip = config['device_ip']
+    router_lan_interface = config['router_lan_interface']
+    net_cond_list = config['net_cond_list']
+    ssh_host = config['ssh_host']
+    ssh_port = config['ssh_port']
+    ssh_username = config['ssh_username']
+    ssh_password = config['ssh_password']
 
-# ssh_client = paramiko.SSHClient()
-# ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# ssh_client.connect(ssh_host, ssh_port, ssh_username, ssh_password)
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(ssh_host, ssh_port, ssh_username, ssh_password)
 
 
-# TODO 自动质差控制
-# net_cond_list = []
+def net_cond_configure(net_cond):
+    rate = net_cond['rate']
+    # 限制下载带宽
+    ssh_client.exec_command(f'tc qdisc add dev {router_lan_interface} root handle 1: htb default 1')
+    ssh_client.exec_command(f'tc class add dev {router_lan_interface} parent 1: classid 1:1 htb rate {rate}Kbit')
+    ssh_client.exec_command(f'tc filter add dev {router_lan_interface} protocol ip parent 1:0 prio 1 u32 match ip dst {device_ip} flowid 1:1')
 
-# def net_cond_configure(net_cond):
-#     # send tc command to up-level router
-#     # ssh to up-level router
-#     # tc command to up-level
-#     ssh_client.exec_command('tc qdisc del dev {} root'.format(net_interface))
-#     ssh_client.exec_command('tc qdisc add dev {} root handle 1: htb'.format(net_interface))
-#     ssh_client.exec_command('tc class add dev {} parent 1: classid 1:1 htb rate {}kbit'.format(net_interface, net_cond['rate']))
+    
+def net_cond_reset():
+    cmd = f'tc qdisc del root dev {router_lan_interface}'
+    ssh_client.exec_command(cmd)
+    
+if not os.path.exists('collection'):
+    os.mkdir('collection')
 
-for play_resolution in play_resolutions:
-    ChromeDriver().play_list(play_list, net_interface=net_interface, play_resolution=play_resolution, fullscreen_play=fullscreen_play)
+for net_cond in net_cond_list:
+    net_cond_reset()
+    net_cond_configure(net_cond)
+    for play_resolution in play_resolutions:
+        ChromeDriver().play_list(play_list, net_interface=net_interface, play_resolution=play_resolution, fullscreen_play=fullscreen_play)
+    # move ouput dir to collection/net_cond
+    net_cond_str = f'rate_{net_cond["rate"]}Kbit'
+    os.rename('output', f'collection/{net_cond_str}')
+net_cond_reset()
--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 import subprocess
 import signal
 import time
+import paramiko
 
-# # 启动tshark进程并抓包
-# tshark_cmd = ['tshark', '-i', 'WLAN', '-w', 'output.pcap']
-# tshark_proc = subprocess.Popen(tshark_cmd,)
+ssh_host = '192.168.31.1'
+ssh_port = 22
+ssh_username = 'root'
+ssh_password = '4bcc8d55'
 
-# time.sleep(2)
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(ssh_host, ssh_port, ssh_username, ssh_password)
 
-# for i in range(2):
-#     # print(i)
-#     time.sleep(1)
-# tshark_proc.send_signal(signal.SIGTERM)
-# tshark_proc.wait()
+device_ip = '192.168.31.56'
+rate = 8000
+router_lan_interface = 'br-lan'
 
-c = 1.1
-# 向上取整
-print(int(c))
+ssh_client.exec_command(f'tc qdisc del root dev {router_lan_interface}')
+ssh_client.exec_command(f'tc qdisc add dev {router_lan_interface} root handle 1: htb default 1')
+ssh_client.exec_command(f'tc class add dev {router_lan_interface} parent 1: classid 1:1 htb rate {rate}Kbit')
+ssh_client.exec_command(f'tc filter add dev {router_lan_interface} protocol ip parent 1:0 prio 1 u32 match ip dst {device_ip} flowid 1:1')
+
+ssh_client.close()
