80e6d7a442d4f7c93b0a624ed4687b50d779e2de
wljyes <1757280079@qq.com>
2023-12-04 11:34:11 +0800

    实现动态带宽限制

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
 from chrome_driver import ChromeDriver
 import paramiko
 import yaml
+import threading
 import os
 
 configure_file = 'config.yaml'
@@ -25,12 +27,17 @@ ssh_client.connect(ssh_host, ssh_port, ssh_username, ssh_password)
 
 def net_cond_configure(net_cond):
     rate = net_cond['rate']
-    # 限制下载带宽
+    duration = net_cond['duration']
     ssh_client.exec_command(f'tc qdisc add dev {router_lan_interface} root handle 1: htb default 1')
-    ssh_client.exec_command(f'tc class add dev {router_lan_interface} parent 1: classid 1:1 htb rate {rate}Kbit')
-    ssh_client.exec_command(f'tc filter add dev {router_lan_interface} protocol ip parent 1:0 prio 1 u32 match ip dst {device_ip} flowid 1:1')
-
+    ssh_client.exec_command(f'tc class add dev {router_lan_interface} parent 1: classid 1:1 htb rate 10MBps')
+    ssh_client.exec_command(f'tc class add dev {router_lan_interface} parent 1: classid 1:2 htb rate {rate}Kbit')
+    ssh_client.exec_command(f'tc filter add dev {router_lan_interface} protocol ip parent 1:0 prio 1 u32 match ip dst {device_ip} flowid 1:2')
+    time.sleep(duration)
+    ssh_client.exec_command(f'tc qdisc del root dev {router_lan_interface}')
     
+def net_cond_configure_thread(net_cond):
+    threading.Timer(net_cond['start_time'], net_cond_configure, args=(net_cond,)).start()
+
 def net_cond_reset():
     cmd = f'tc qdisc del root dev {router_lan_interface}'
     ssh_client.exec_command(cmd)
@@ -39,10 +46,11 @@ if not os.path.exists('collection'):
     os.mkdir('collection')
 
 for net_cond in net_cond_list:
-    net_cond_reset()
-    net_cond_configure(net_cond)
     for play_resolution in play_resolutions:
-        ChromeDriver().play_list(play_list, net_interface=net_interface, play_resolution=play_resolution, fullscreen_play=fullscreen_play)
+        for ip in play_list:
+            net_cond_reset()
+            net_cond_configure(net_cond)
+            ChromeDriver().play_one(ip, net_interface=net_interface, play_resolution=play_resolution, fullscreen_play=fullscreen_play)
     # move ouput dir to collection/net_cond
     net_cond_str = f'rate_{net_cond["rate"]}Kbit'
     os.rename('output', f'collection/{net_cond_str}')
